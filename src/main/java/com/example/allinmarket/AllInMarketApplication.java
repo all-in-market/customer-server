d60ada7509c4ee0c85d190a8d5f4a332d2d995cd
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-08 21:14:07 +0900

    chore : 의존성 추가

---
+++ src/main/java/com/example/allinmarket/AllInMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.allinmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/allinmarket/AllInMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allinmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllInMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

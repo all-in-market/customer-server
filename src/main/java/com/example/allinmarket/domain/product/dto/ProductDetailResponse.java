2951acfd5cd50f47e12442d9d132d374cf1e25cf
Leejun14 <777ljy777@gmail.com>
2026-04-10 20:25:22 +0900

    refactor : 상품 반환 dto 수정

--- src/main/java/com/example/allinmarket/domain/product/dto/ProductDetailResponse.java
+++ src/main/java/com/example/allinmarket/domain/product/dto/ProductDetailResponse.java
@@ -1,15 +1,14 @@
 package com.example.allinmarket.domain.product.dto;
 
-import com.example.allinmarket.domain.category.entity.Category;
 import com.example.allinmarket.domain.product.entity.Product;
 import com.example.allinmarket.domain.product.enums.ProductStatus;
-import com.example.allinmarket.seller.entity.Seller;
 
 import java.math.BigDecimal;
 
 public record ProductDetailResponse (
-        Seller seller,
-        Category category,
+
+        Long sellerId,
+        Long categoryId,
         String name,
         BigDecimal price,
         int stock,
@@ -18,8 +17,8 @@ public record ProductDetailResponse (
 ) {
     public static ProductDetailResponse from(Product product) {
         return new ProductDetailResponse(
-                product.getSeller(),
-                product.getCategory(),
+                product.getSeller().getId(),
+                product.getCategory().getId(),
                 product.getName(),
                 product.getPrice(),
                 product.getStock(),

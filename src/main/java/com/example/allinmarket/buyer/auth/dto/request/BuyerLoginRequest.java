d4d9b3c7809c69502444dfa2e79d04f2ce8b17b4
bjw446 <bjw446@naver.com>
2026-04-10 21:04:20 +0900

    refactor : 로그인 시 이메일 사이즈 추가

--- src/main/java/com/example/allinmarket/buyer/auth/dto/request/BuyerLoginRequest.java
+++ src/main/java/com/example/allinmarket/buyer/auth/dto/request/BuyerLoginRequest.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 public record BuyerLoginRequest(
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         @NotBlank(message = "이메일은 필수 입력 사항 입니다.")
+        @Size(max = 100, message = "이메일은 최대 100자 까지 입력 가능합니다.")
         String email,
 
         @NotBlank(message = "비밀번호는 필수 입력 사항 입니다.")

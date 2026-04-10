82074ce6fa9596fe36165fb5f3ea90329b647656
Leejun14 <777ljy777@gmail.com>
2026-04-10 21:30:44 +0900

    refactor : 판매자 상품 생성 서비스 수정

--- src/main/java/com/example/allinmarket/seller/product/service/SellerProductService.java
+++ src/main/java/com/example/allinmarket/seller/product/service/SellerProductService.java
@@ -3,6 +3,8 @@ package com.example.allinmarket.seller.product.service;
 import com.example.allinmarket.common.enums.ErrorEnum;
 import com.example.allinmarket.common.exception.BaseException;
 import com.example.allinmarket.common.security.SecurityUtils;
+import com.example.allinmarket.domain.category.entity.Category;
+import com.example.allinmarket.domain.category.repository.CategoryRepository;
 import com.example.allinmarket.domain.product.dto.ProductDetailResponse;
 import com.example.allinmarket.domain.product.entity.Product;
 import com.example.allinmarket.domain.product.repository.ProductRepository;
@@ -18,19 +20,23 @@ import org.springframework.stereotype.Service;
 public class SellerProductService {
     private final SellerRepository sellerRepository;
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     public ProductDetailResponse create(@Valid SellerProductCreateRequest request) {
         Long sellerId = SecurityUtils.getCurrentUserId();
         Seller seller = sellerRepository.findById(sellerId).orElseThrow(
                 () -> new BaseException(ErrorEnum.USER_NOT_FOUND)
         );
+        Category category = categoryRepository.findById(request.categoryId()).orElseThrow(
+                () -> new BaseException(ErrorEnum.CATEGORY_NOT_FOUND)
+        );
+
         Product product = Product.of(
                 seller,
-                request.category(),
+                category,
                 request.name(),
                 request.price(),
                 request.stock(),
-                request.status(),
                 request.description()
         );
 

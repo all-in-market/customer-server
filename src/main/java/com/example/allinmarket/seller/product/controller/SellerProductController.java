c8035ff9e63a456b8622547ae92acb611ba0cc98
Leejun14 <777ljy777@gmail.com>
2026-04-10 20:42:20 +0900

    refactor : 판매자 상품 생성 컨트롤러 구현

--- src/main/java/com/example/allinmarket/seller/product/controller/SellerProductController.java
+++ src/main/java/com/example/allinmarket/seller/product/controller/SellerProductController.java
@@ -2,6 +2,8 @@ package com.example.allinmarket.seller.product.controller;
 
 import com.example.allinmarket.common.enums.SuccessEnum;
 import com.example.allinmarket.common.response.ApiResponse;
+import com.example.allinmarket.domain.product.dto.ProductDetailResponse;
+import com.example.allinmarket.seller.product.dto.request.SellerProductCreateRequest;
 import com.example.allinmarket.seller.product.service.SellerProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,22 +18,22 @@ public class SellerProductController {
     private final SellerProductService sellerProductService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<SellerProductResponse>> create(@Valid @RequestBody SellerProductCreateRequest request) {
+    public ResponseEntity<ApiResponse<ProductDetailResponse>> create(@Valid @RequestBody SellerProductCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(SuccessEnum.CREATE_SUCCESS, sellerProductService.create(request)));
     }
 
-    @PutMapping("/{productId}")
-    public ResponseEntity<ApiResponse<SellerProductResponse>> update(@PathVariable(name = "productId") Long productId, @Valid @RequestBody SellerProductUpdateRequest request) {
-        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.UPDATE_SUCCESS, sellerProductService.update(productId, request)));
-    }
-
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<ApiResponse<SellerProductResponse>> delete(@PathVariable(name = "productId") Long productId) {
-        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.DELETE_SUCCESS, sellerProductService.delete(productId)));
-    }
-
-    @PutMapping("/{productId}/stock")
-    public ResponseEntity<ApiResponse<SellerProductResponse>> stockUpdate(@PathVariable(name = "productId") Long productId, @Valid @RequestBody SellerProductStockUpdateRequest request) {
-        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.UPDATE_SUCCESS, sellerProductService.stockUpdate(productId, request)));
-    }
+//    @PutMapping("/{productId}")
+//    public ResponseEntity<ApiResponse<ProductDetailResponse>> update(@PathVariable(name = "productId") Long productId, @Valid @RequestBody SellerProductUpdateRequest request) {
+//        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.UPDATE_SUCCESS, sellerProductService.update(productId, request)));
+//    }
+//
+//    @DeleteMapping("/{productId}")
+//    public ResponseEntity<ApiResponse<ProductDetailResponse>> delete(@PathVariable(name = "productId") Long productId) {
+//        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.DELETE_SUCCESS, sellerProductService.delete(productId)));
+//    }
+//
+//    @PutMapping("/{productId}/stock")
+//    public ResponseEntity<ApiResponse<ProductDetailResponse>> stockUpdate(@PathVariable(name = "productId") Long productId, @Valid @RequestBody SellerProductStockUpdateRequest request) {
+//        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.UPDATE_SUCCESS, sellerProductService.stockUpdate(productId, request)));
+//    }
 }

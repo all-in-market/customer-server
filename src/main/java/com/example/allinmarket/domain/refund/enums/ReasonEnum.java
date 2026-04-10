a010c58df4777fa8546f0008ab99c1bd0f827807
Leejun14 <777ljy777@gmail.com>
2026-04-10 12:23:13 +0900

    feat : refund enum 구현

---
+++ src/main/java/com/example/allinmarket/domain/refund/enums/ReasonEnum.java
@@ -0,0 +1,8 @@
+package com.example.allinmarket.domain.refund.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ReasonEnum {
+    CHANGE_OF_MINE, WRONG_ITEM, DAMAGED
+}
---
+++ src/main/java/com/example/allinmarket/domain/refund/enums/RefundStatus.java
@@ -0,0 +1,8 @@
+package com.example.allinmarket.domain.refund.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum RefundStatus {
+    REQUESTED, APPROVED, REJECTED
+}

45ad6884eabcd01c168d96ddb3db7f6caecf57fe
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-10 12:49:34 +0900

    feat : CartItem 엔티티 작성

--- src/main/java/com/example/allinmarket/domain/cartitem/entity/CartItem.java
+++ src/main/java/com/example/allinmarket/domain/cartitem/entity/CartItem.java
@@ -1,4 +1,50 @@
 package com.example.allinmarket.domain.cartitem.entity;
 
-public class CartItem {
+import com.example.allinmarket.common.entity.DeletableEntity;
+import com.example.allinmarket.domain.cart.entity.Cart;
+import com.example.allinmarket.domain.product.entity.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CartItem extends DeletableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity = 1;
+
+    public static CartItem of(Cart cart, Product product) {
+        CartItem cartItem = new CartItem();
+        cartItem.cart = cart;
+        cartItem.product = product;
+        cartItem.quantity = 1;
+
+        return cartItem;
+    }
+
+    public void softDelete() {
+        /**
+         * 삭제 전 검증 로직
+         */
+        delete();
+    }
 }

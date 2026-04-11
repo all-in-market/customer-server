99f2c93d9a84eb7e94a21766416341dac15b6cb1
hyu1335 <hyuham1335@gmail.com>
2026-04-11 14:46:12 +0900

    refactor: 불필요한 의존성 제거

--- src/main/java/com/example/allinmarket/buyer/repository/BuyerRepository.java
+++ src/main/java/com/example/allinmarket/buyer/repository/BuyerRepository.java
@@ -1,11 +1,8 @@
 package com.example.allinmarket.buyer.repository;
 
 import com.example.allinmarket.buyer.entity.Buyer;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {

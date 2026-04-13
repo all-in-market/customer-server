fa84267d0d24e8e9dd05c2835462cf3124450a39
LeeJun14 <777ljy777@gmail.com>
2026-04-13 15:16:39 +0900

    feat: 판매자 판매 상품 목록 조회 구현

--- src/main/java/com/example/allinmarket/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/allinmarket/domain/product/repository/ProductRepository.java
@@ -29,4 +29,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("SELECT p FROM Product p WHERE p.status != 'HIDDEN' AND p.deletedAt IS NULL")
     Optional<Product> findVisibleProductById(Long productId);
+
+    Page<Product> findAllBySellerIdAndDeletedAtIsNull(Long sellerId, Pageable pageable);
 }

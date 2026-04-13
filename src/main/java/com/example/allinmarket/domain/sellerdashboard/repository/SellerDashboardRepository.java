d30d644fb4c31f6bcb7938c7c12a641a182a80d4
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-13 11:04:57 +0900

    fix : 대시보드 레포지토리 Jpa레포지토리로 변경

--- src/main/java/com/example/allinmarket/domain/sellerdashboard/repository/SellerDashboardRepository.java
+++ src/main/java/com/example/allinmarket/domain/sellerdashboard/repository/SellerDashboardRepository.java
@@ -1,11 +1,11 @@
 package com.example.allinmarket.domain.sellerdashboard.repository;
 
 import com.example.allinmarket.domain.sellerdashboard.entity.SellerDashboard;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface SellerDashboardRepository extends CrudRepository<SellerDashboard, Long> {
+public interface SellerDashboardRepository extends JpaRepository<SellerDashboard, Long> {
     Optional<SellerDashboard> findBySellerIdAndStatDate(Long id, LocalDate day);
 }

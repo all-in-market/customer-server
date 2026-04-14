de4933df16d034c09f8566c095a1268d44047594
LeeJun14 <777ljy777@gmail.com>
2026-04-14 12:59:33 +0900

    refactor: 로그 기록 수정

--- src/main/java/com/example/allinmarket/common/config/SellerInitializer.java
+++ src/main/java/com/example/allinmarket/common/config/SellerInitializer.java
@@ -46,6 +46,6 @@ public class SellerInitializer implements ApplicationRunner {
         seller.updateStatus(SellerStatus.APPROVED);
         sellerRepository.save(seller);
 
-        log.info("슈퍼 관리자 계정이 생성되었습니다: {}", email);
+        log.info("판매자 계정이 생성되었습니다: {}", email);
     }
 }

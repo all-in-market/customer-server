a1b6a094f81fecb469dcebf1a95498a62f893e89
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-16 19:13:26 +0900

    feat : 판매자 통계 조회 컨트롤러 메서드 작성

--- src/main/java/com/example/allinmarket/seller/dailyStatistics/controller/SellerDailyStatisticsController.java
+++ src/main/java/com/example/allinmarket/seller/dailyStatistics/controller/SellerDailyStatisticsController.java
@@ -1,8 +1,15 @@
 package com.example.allinmarket.seller.dailyStatistics.controller;
 
+import com.example.allinmarket.common.enums.SuccessEnum;
+import com.example.allinmarket.common.response.ApiResponse;
+import com.example.allinmarket.common.security.SecurityUtils;
+import com.example.allinmarket.domain.sellerdailystatistics.dto.DailyStatisticsResponse;
 import com.example.allinmarket.domain.sellerdailystatistics.repository.SellerDailyStatisticsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/seller/statistics")
 public class SellerDailyStatisticsController {
 
-    private final SellerDailyStatisticsRepository sellerDailyStatisticsRepository;
+    private final SellerDailyStatisticsService sellerDailyStatisticsService;
 
     // 특정일 조회
     ///  TODO : 특정일 조회
@@ -18,4 +25,17 @@ public class SellerDailyStatisticsController {
 
     // 특정 기간 조회
     ///  TODO : 특정 기간 조회
+    public ResponseEntity<ApiResponse<DailyStatisticsResponse>> getRangedStatistics(
+            @RequestParam String from,
+            @RequestParam String to
+    ) {
+        Long sellerId = SecurityUtils.getCurrentUserId();
+        return ResponseEntity.ok(ApiResponse.success(
+                SuccessEnum.LOGIN_SUCCESS,
+                sellerDailyStatisticsService.getRangedStatistics(
+                        sellerId, from, to
+                )
+                )
+        );
+    }
 }

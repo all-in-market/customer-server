280d9a9735f92601a5ab120d178a48d630bfb5c8
bjw446 <bjw446@naver.com>
2026-04-16 11:12:46 +0900

    chore : 개행 수정

--- src/main/java/com/example/allinmarket/common/redis/RedisConfig.java
+++ src/main/java/com/example/allinmarket/common/redis/RedisConfig.java
@@ -14,8 +14,7 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template =
-                new RedisTemplate<>();
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
 
         template.setConnectionFactory(redisConnectionFactory);
 

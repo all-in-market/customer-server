66d756a5f2bb9d64d8c8832cab29393ac30d447c
hyu1335 <hyuham1335@gmail.com>
2026-04-27 14:27:21 +0900

    feat: main 브랜치에 push시 자동배포가 되도록 추가

--- infra/github_actions_iam.tf
+++ infra/github_actions_iam.tf
@@ -21,7 +21,8 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
       values = [
-        "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+        for b in var.github_branches :
+        "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/${b}"
       ]
     }
   }
